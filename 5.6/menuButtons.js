70976dbb36f512ad1b2ad007b2e74ef5d4d12cef
johnmacleod <john.macleod84@gmail.com>
2012-11-08 13:14:30 -0500

    Added stuff:
    - Included favicon for Blackbird Cafe
    - Added script to change selected menu item for Blackbird Cafe's Coffee
    & Tea page
    - Added Blackbird Cafe Coffee & tea page

---
+++ 5.6/menuButtons.js
@@ -0,0 +1,89 @@
+// JavaScript Document
+//	var homeP = 'Vintage Airplane Tshirts
+
+
+	var coffeeT = "Blackbird Cafe | Coffee & Tea"  /* Title text */
+	var landing = "Blackbird Cafe"
+
+	if ( (document.title) == coffeeT ) {
+				$('.left-horz-menu > li > a').eq(3).css('line-height', '26px');
+				$('.left-horz-menu > li > a').eq(3).css('border-top-color', '#000');
+				$('.left-horz-menu > li > a').eq(3).css('border-top-width', '1px');
+				$('.left-horz-menu > li > a').eq(3).css('background-color', '#fff');
+				$('.left-horz-menu > li > a').eq(3).css('color', '#000');
+				$('.left-horz-menu > li > a').eq(3).css('border-top-style', 'solid');
+				$('.left-horz-menu > li > a').eq(3).css('border-top-color', '#000');
+				$('.left-horz-menu > li > a').eq(3).css('border-top-width', '2px');
+				$('.left-horz-menu > li > a').eq(3).css('border-bottom-style', 'solid');
+				$('.left-horz-menu > li > a').eq(3).css('border-bottom-color', '#000');
+				$('.left-horz-menu > li > a').eq(3).css('border-bottom-width', '2px');
+				
+	} else if ( (document.title) == landing ) {
+
+	}
+
+
+
+
+
+	/*
+
+	if ( (document.title) == coffeeT ) {
+		$('.upper_nav > ul > li').eq(0).css('background-color', '#470606');
+		$('.upper_nav > ul > li').eq(0).css('border-right-width','2px');	
+		$('.upper_nav > ul > li').eq(0).css('border-right-style','solid');			
+		$('.upper_nav > ul > li').eq(0).css('border-right-color','#933939');
+		$('.upper_nav > ul > li').eq(0).css('border-left-width','1px');	
+		$('.upper_nav > ul > li').eq(0).css('border-left-style','colors');			
+		$('.upper_nav > ul > li').eq(0).css('border-left-color','#930C0C');
+	} else if ( (document.title) == aboutP ) {
+		$('.upper_nav > ul > li').eq(1).css('background-color', '#470606');
+		//alert("That ain't it!");
+	}
+	
+	
+	switch (document.title) {
+		case photosP:
+			$('.upper_nav > ul > li > a').eq(0).css('background-color', '#470606');
+			$('.upper_nav > ul > li > a').eq(0).css('border-right-width','1px');	
+			$('.upper_nav > ul > li > a').eq(0).css('border-right-style','solid');			
+			$('.upper_nav > ul > li > a').eq(0).css('border-right-color','#933939');
+			$('.upper_nav > ul > li > a').eq(0).css('border-left-width','1px');	
+			$('.upper_nav > ul > li > a').eq(0).css('border-left-style','colors');			
+			$('.upper_nav > ul > li > a').eq(0).css('border-left-color','#930C0C');
+			break;
+			
+		case aboutP:
+			$('.upper_nav > ul > li > a').eq(1).css('background-color', '#470606');
+			$('.upper_nav > ul > li > a').eq(1).css('border-right-width','1px');	
+			$('.upper_nav > ul > li > a').eq(1).css('border-right-style','solid');			
+			$('.upper_nav > ul > li > a').eq(1).css('border-right-color','#933939');
+			$('.upper_nav > ul > li > a').eq(1).css('border-left-width','1px');	
+			$('.upper_nav > ul > li > a').eq(1).css('border-left-style','colors');			
+			$('.upper_nav > ul > li > a').eq(1).css('border-left-color','#930C0C');
+			break;
+			
+		case linksP:
+			$('.upper_nav > ul > li > a').eq(2).css('background-color', '#470606');
+			$('.upper_nav > ul > li > a').eq(2).css('border-right-width','1px');	
+			$('.upper_nav > ul > li > a').eq(2).css('border-right-style','solid');			
+			$('.upper_nav > ul > li > a').eq(2).css('border-right-color','#933939');
+			$('.upper_nav > ul > li > a').eq(2).css('border-left-width','1px');	
+			$('.upper_nav > ul > li > a').eq(2).css('border-left-style','colors');			
+			$('.upper_nav > ul > li > a').eq(2).css('border-left-color','#930C0C');
+			break;
+			
+		case viewCartP:
+			$('.upper_nav > ul > li > a').eq(3).css('background-color', '#470606');
+			$('.upper_nav > ul > li > a').eq(3).css('border-right-width','1px');	
+			$('.upper_nav > ul > li > a').eq(3).css('border-right-style','solid');			
+			$('.upper_nav > ul > li > a').eq(3).css('border-right-color','#933939');
+			$('.upper_nav > ul > li > a').eq(3).css('border-left-width','1px');	
+			$('.upper_nav > ul > li > a').eq(3).css('border-left-style','colors');			
+			$('.upper_nav > ul > li > a').eq(3).css('border-left-color','#930C0C');
+			break;						
+			
+		default:
+		
+	}
+	*/
